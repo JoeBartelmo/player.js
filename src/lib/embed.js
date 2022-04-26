1505274e8fb89ed70e60b3e79a150ecd536a8c11
Joe Bartelmo <joebartelmo@gmail.com>
2022-04-26 14:28:09 -0400

    pls work

--- src/lib/embed.js
+++ src/lib/embed.js
@@ -89,7 +89,7 @@ export function getOEmbedData(videoUrl, params = {}, element) {
             throw new TypeError(`“${videoUrl}” is not a vimeo.com url.`);
         }
 
-        let url = `https://vimeo.com/api/oembed.json?url=${encodeURIComponent(videoUrl)}`;
+        let url = `/api/proxy/vimeo.com/api/oembed.json?url=${encodeURIComponent(videoUrl)}`;
 
         for (const param in params) {
             if (params.hasOwnProperty(param)) {
